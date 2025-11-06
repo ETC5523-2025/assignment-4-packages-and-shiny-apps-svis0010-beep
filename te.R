6ae0526d436739e6124e6d7c24d96ac1fb4cb8ee
Siddhi Vispute <svis0010@student.monash.edu>
2025-11-06 14:52:31 +1100

    Update FWI dataset and Shiny app

--- te.R
+++ te.R
@@ -5,5 +5,17 @@ mydata <- read_excel("data-raw/fwi.xlsx")
 usethis::use_data(mydata, overwrite = TRUE)
 usethis::use_data(fwi, overwrite = TRUE)
 
-devtools::document()   # creates documentation
+devtools::document() # creates documentation
 devtools::load_all()
+
+devtools::install()
+
+
+devtools::document()  # Update documentation
+devtools::install()
+
+library(myPackage)
+launch_app()
+system("git add .")
+system('git commit -m "Update FWI dataset and Shiny app"')
+system("git push origin main")
